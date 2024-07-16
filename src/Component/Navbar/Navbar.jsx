ec8c3d56a41b8fe5c6683120057b2e6f7d83343e
harshtrivedi01 <harsh2trivedi786@gmail.com>
2024-07-16 18:21:59 +0530

    resume builder intergrated

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -85,6 +85,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-black border-b border-gray-200">
+      
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0 flex items-center">
